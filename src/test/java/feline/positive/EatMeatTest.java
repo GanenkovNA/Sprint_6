93c53fe599f9a58e8c175a33f0a59e42ca706045
Nikita Ganenkov <ganenkov.n.a@gmail.com>
2025-05-15 21:56:51 +0300

    Удалил тест с моком в `EatMeatTest`

--- src/test/java/feline/positive/EatMeatTest.java
+++ src/test/java/feline/positive/EatMeatTest.java
@@ -1,31 +1,12 @@
 package feline.positive;
 
-import com.example.Feline;
 import feline.FelineBase;
 import org.junit.Test;
 
-import java.util.List;
-
 import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.*;
 import static resources.VariablesForTests.PREDATOR_FOOD_LIST;
-import static resources.VariablesForTests.PREDATOR_VALUE;
 
 public class EatMeatTest extends FelineBase {
-
-    // Контрактный тест
-    @Test
-    public void shouldCallGetFoodWithPredatorValue() throws Exception {
-        feline = spy(new Feline());
-
-        doReturn(List.of(PREDATOR_FOOD_LIST))
-                .when(feline)
-                .getFood(PREDATOR_VALUE);
-        feline.eatMeat();
-        verify(feline).getFood(eq(PREDATOR_VALUE));
-    }
-
     // Интеграционный тест
     @Test
     public void shouldReturnPredatorFoodList() throws Exception {
